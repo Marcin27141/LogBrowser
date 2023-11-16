47393a02a00500a5777868999e104dd78b3d3e09
Marcin <marcin.tkocz@op.pl>
2023-11-16 11:26:12 +0100

    added file opener widget

--- View/OpenFileWidget.py
+++ View/OpenFileWidget.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QLabel
+from PySide6.QtWidgets import QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QLabel, QFileDialog
+import os
 
 class OpenFileWidget(QWidget):
     def __init__(self, controller, logs_list_component) -> None:
@@ -7,18 +8,29 @@ class OpenFileWidget(QWidget):
         self.logs_list_component = logs_list_component
         self.filepath_input = QLineEdit()
         self.open_button = QPushButton("Open")
+        self.select_button = QPushButton("Select")
         self.not_found_label = QLabel()
         self.setLayout(self.get_widget_layout())
         self.open_button.clicked.connect(self.try_load_file)
+        self.select_button.clicked.connect(self.select_file)
 
     def get_widget_layout(self):
         widget_layout = QVBoxLayout()
         upper_layout = QHBoxLayout()
         upper_layout.addWidget(self.filepath_input)
         upper_layout.addWidget(self.open_button)
+        upper_layout.addWidget(self.select_button)
         widget_layout.addLayout(upper_layout)
         widget_layout.addWidget(self.not_found_label)
         return widget_layout
+    
+    def select_file(self):
+        response, _ = QFileDialog.getOpenFileName(
+            parent=self.logs_list_component,
+            caption='Select a log file'
+        )
+        self.filepath_input.setText(str(response))
+        self.try_load_file()
 
     def try_load_file(self):
         filepath = self.filepath_input.text()
@@ -27,3 +39,4 @@ class OpenFileWidget(QWidget):
         else:
             self.not_found_label.clear()
             self.logs_list_component.initialize_list(filepath)
+        
